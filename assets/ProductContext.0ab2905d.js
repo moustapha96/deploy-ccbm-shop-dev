56f41ace1c0a1045bd31c1f8bdeca65df1ab389c
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-03-10 11:27:25 +0000

    mise a jour 10_03_2025
    - icon WhatsApp au lieu de nous parler
    
    - numero de telephone toujours visible
    
    - Tout les produits )=> catgeories
    
    - Boutique: deux produits a la fois

--- assets/ProductContext.0ab2905d.js
+++
@@ -1,1 +0,0 @@
-import"./index.bdb91bf9.js";import{r as t}from"./vendor.ee8c1fef.js";const e=t.createContext();export{e as P};
--- assets/ProductContext.29dabe53.js
+++
@@ -1,1 +0,0 @@
-import"./index.af7f2420.js";import{r as t}from"./vendor.ee8c1fef.js";const e=t.createContext();export{e as P};
