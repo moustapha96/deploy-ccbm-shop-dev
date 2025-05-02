53063244622ff7ca660da2b3f45c8a949d04ec2e
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-05-02 16:02:31 +0000

    addd

---
+++ assets/DataIteration.d009a04b.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.d85d8073.js";function c(e){const{datas:t=[],startLength:r,endLength:n,children:s}=e;return a.jsx(a.Fragment,{children:t&&t.length>=n&&t.slice(r,n).map(i=>s({datas:i}))})}export{c as D};
