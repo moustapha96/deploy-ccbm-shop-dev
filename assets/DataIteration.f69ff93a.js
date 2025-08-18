4931daf67a3eb460b7530e880bd9fac9e13ac12f
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-08-18 07:54:16 +0000

    delete button navigation

---
+++ assets/DataIteration.f69ff93a.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.22a9aa87.js";function c(e){const{datas:t=[],startLength:r,endLength:n,children:s}=e;return a.jsx(a.Fragment,{children:t&&t.length>=n&&t.slice(r,n).map(i=>s({datas:i}))})}export{c as D};
