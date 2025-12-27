02c4f6ff55cd8bfd6fbf9e62b76152aa1beb2408
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2025-12-27 21:29:23 +0000

    update

---
+++ assets/js/DataIteration-CLuM4tx-.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D2o1u-UB.js";import{R as d}from"./vendor-DZBautpX.js";function h(s){const{datas:n=[],startLength:a,endLength:e,children:i}=s;return r.jsx(r.Fragment,{children:n&&n.length>=e&&n.slice(a,e).map((t,o)=>{const c=t&&t.id||t&&t.slug||`data-iteration-${a+o}`;return r.jsx(d.Fragment,{children:i({datas:t})},c)})})}export{h as D};
---
+++ assets/js/UserContext-DUTHnAVe.js
@@ -0,0 +1,1 @@
+import"./index-D2o1u-UB.js";import{b as t}from"./vendor-DZBautpX.js";t.createContext();
